88ac3e537607f4ac6ab6cb595006e5c4210ba075
AdyaS <1810adya@gmail.com>
2024-07-11 23:10:57 +1200

    adding req files

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT5503_Assignment2_2230449
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+            bool continueShopping = true;
+
+            while (continueShopping)
+            {
+                Console.WriteLine("\n**********************Latest Car Dealership Business!**********************");
+                Console.WriteLine("\n1. Suzuki $7000");
+                Console.WriteLine("2. Hondal $8000");
+                Console.WriteLine("3. Tesla $9000");
+                Console.WriteLine("Please enter your choice of car");
+                Console.WriteLine("Further details- GST and Total Price can be viewed based on your choice");
+                Console.WriteLine("***************************************************************************");
+
+                int choice;
+                if (int.TryParse(Console.ReadLine(), out choice))
+                {
+                    double price = 0;
+                    switch (choice)
+                    {
+                        case 1:
+                            price = 7000;
+                            break;
+                        case 2:
+                            price = 8000;
+                            break;
+                        case 3:
+                            price = 9000;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong choice. Please enter a valid choice.");
+                            continue;
+                    }
+
+                    double gst = price * 0.15;
+                    double total = price + gst;
+
+                    Console.WriteLine($"The GST is: {gst}");
+                    Console.WriteLine($"And the Total Price is: {total}");
+
+                    Console.WriteLine("Do you wish to continue shopping? (Y/N)");
+                    char response = char.ToUpper(Console.ReadKey().KeyChar);
+                    if (response == 'N')
+                    {
+                        continueShopping = false;
+                        Console.WriteLine("\nHave a nice day!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid choice.");
+                }
+            }
+        }
+        }
+
+
+}
+
+
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IT5503_Assignment2_2230449")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wellington Institute of Technology")]
+[assembly: AssemblyProduct("IT5503_Assignment2_2230449")]
+[assembly: AssemblyCopyright("Copyright © Wellington Institute of Technology 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("557bf0d3-877c-42f7-b814-c7150a2acec8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
